379c6e69f41c25a479a1f8f8c031ca91e9911bc0
Jake Brunner <36394863+JAndrew13@users.noreply.github.com>
2025-02-22 15:58:39 -0800

    update launch settings

--- Server/Program.cs
+++ Server/Program.cs
@@ -10,6 +10,10 @@ using System.Text;
 
 
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    serverOptions.ListenAnyIP(8080);
+});
 
 
 var settings = new Settings();
